109b29552883f0d270cf2e3cb874efaa3ab1144e
Jerry Shao <jshao@apache.org>
2026-02-06 14:17:27 +0800

    Polish the code and add validation logic

--- tests/test_basic.py
+++
@@ -1,8 +0,0 @@
-"""Basic tests for adp_hypervisor package."""
-
-from adp_hypervisor import __version__
-
-
-def test_version() -> None:
-    """Test that version is defined."""
-    assert __version__ == "0.1.0"
